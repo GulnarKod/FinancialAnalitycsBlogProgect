5217f777454fe1a4c9076819d057af96fa90dc26
Gulnar Kodzhaeva <gulnarkodzhaeva@gmail.co>
2024-01-18 09:07:02 +0300

    add firebase

---
+++ src/firebase/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import 'firebase/firebase-firestore'
+import 'firebase/firebase-auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTDYymf4fArPL2HSJf0TpKYaxV3iwLlms",
+  authDomain: "financialmarketnewsdb.firebaseapp.com",
+  projectId: "financialmarketnewsdb",
+  storageBucket: "financialmarketnewsdb.appspot.com",
+  messagingSenderId: "766129744344",
+  appId: "1:766129744344:web:13e73c377f31c7681faaad"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
